937da50bbba8f6a8585cf7650fbce53b9849db2a
Juan FEMPA <juan.fempa@gmail.com>
2022-06-08 18:22:32 +0200

    Terminada

--- src/components/SioNo.vue
+++ src/components/SioNo.vue
@@ -1,14 +1,14 @@
 <template>
-  <img src="https://via.placeholder.com/250" alt="fondo">
+  <!-- <img src="https://via.placeholder.com/250" alt="fondo" /> -->
+  <img v-if="urlImg" :src="urlImg" alt="fondo" />
   <!-- Fondo que oscurece toda la pantalla -->
   <div class="bg-dark"></div>
   <div class="indecision-container">
-    <input type="text" placeholder="¿Qué quieres saber?"
-        v-model="pregunta">
+    <input type="text" placeholder="¿Qué quieres saber?" v-model="pregunta" />
     <p>Recuerda terminar con un signo de interrogación (?).</p>
     <div>
-      <h2>¿Seré millonario?</h2>
-      <h1>Sí, No, Pensando...</h1>
+      <h2>{{ preguntaCorrecta }}</h2>
+      <h1>{{ respuesta }}</h1>
     </div>
   </div>
 </template>
@@ -16,54 +16,71 @@
 
 <script>
 export default {
- data() {
-  return {
-   pregunta: ''
-  }
- },
- methods: {
-  preguntar() {
-   console.log(pregunta);
-  }
- }
-}
+  data() {
+    return {
+      pregunta: "",
+      preguntaCorrecta: null,
+      respuesta: null,
+      urlImg: null,
+    };
+  },
+  methods: {
+    async obtenerRespuesta() {
+      this.respuesta = "Pensando...";
+      const { answer, image } = await fetch("https://yesno.wtf/api").then(
+        (respuesta) => respuesta.json()
+      );
+      this.respuesta = answer;
+      this.urlImg = image;
+    },
+  },
+  watch: {
+    pregunta(nuevoValor) {
+      if (!nuevoValor.endsWith("?")) return;
+      this.preguntaCorrecta = nuevoValor;
+      this.obtenerRespuesta();
+    },
+  },
+};
 </script>
 
 <style>
-img, .bg-dark {
- height: 100vh;
- left: 0px;
- max-height: 100%;
- max-width: 100%;
- position: fixed;
- top: 0px;
- width: 100vw;
+img,
+.bg-dark {
+  height: 100vh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
 }
 .bg-dark {
- background-color: rgba(0, 0, 0, 0.4);
+  background-color: rgba(0, 0, 0, 0.4);
 }
 .indecision-container {
- position: relative;
- z-index: 99;
+  position: relative;
+  z-index: 99;
 }
 input {
- border-radius: 5px;
- border: none;
- padding: 10px 15px;
- width: 250px;
+  border-radius: 5px;
+  border: none;
+  padding: 10px 15px;
+  width: 250px;
 }
 input:focus {
- outline: none;
+  outline: none;
 }
 p {
- color: white;
- font-size: 20px;
- margin-top: 0px;
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
 }
-h1,h2 {
- color: white;
+h1,
+h2 {
+  color: white;
 }
 h2 {
- margin-top: 150px;
+  margin-top: 150px;
 }
 </style>
